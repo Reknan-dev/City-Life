d53657cd51e9f40ee81f40e398c4b74403781972
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-07-31 15:13:34 +0200

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: {
-    index: '/src/index.js'
+    index: ['/src/index.js', '/src/css/style.css'],
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
-  module: { rules: [] },
+  module: { rules: [{
+   test:  /\.css$/,
+   use: [
+    MiniCssExtractPlugin.loader,
+    'css-loader',
+  ],
+  }] },
   plugins: [
     new HtmlWebpackPlugin({ 
       title: 'City Life',
       template: './src/index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
     })
   ],
   devServer: {
