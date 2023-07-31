046470004dc3d951ed650f9b3cbe9e082118664e
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-07-31 15:28:31 +0200

    webpack for css

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,40 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    index: ['/src/index.js', '/src/css/style.css'],
+    index: ["/src/index.js", "/src/css/style.css"],
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
-  module: { rules: [{
-   test:  /\.css$/,
-   use: [
-    MiniCssExtractPlugin.loader,
-    'css-loader',
-  ],
-  }] },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
   plugins: [
-    new HtmlWebpackPlugin({ 
-      title: 'City Life',
-      template: './src/index.html'
+    new HtmlWebpackPlugin({
+      title: "City Life",
+      template: "./src/index.html",
     }),
     new MiniCssExtractPlugin({
-      filename: '[name].css'
-    })
+      filename: "[name].css",
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
+    }),
   ],
   devServer: {
     port: 5000,
     open: true,
-    static: path.resolve(__dirname, 'dist')
+    static: path.resolve(__dirname, "dist"),
   },
-  mode: 'production'
-}
-
+  mode: "production",
+};
